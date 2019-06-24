f916bf04b44b735f6ae2df86fb7e61f9db5b838b
arjuncr <arjuncr00@gmail.com>
2019-06-24 21:15:38 +0530

    fix in script

--- build-script/build_linux.sh
+++ build-script/build_linux.sh
@@ -225,10 +225,6 @@ build_kernel () {
 
     cp $LIGHT_OS_KCONFIG .config
     
-    #make module ROSS_COMPILE=$CROSS_COMPILE64 ARCH=$ARCH64 -j ${JFLAG}
-
-    #make modules_install CROSS_COMPILE=$CROSS_COMPILE64 ARCH=$ARCH64 -j ${JFLAG}
-
     make CROSS_COMPILE=$CROSS_COMPILE64 ARCH=$ARCH64 bzImage \
         -j ${JFLAG}
      cp arch/$ARCH64/boot/bzImage ${ISODIR}/kernel.gz
@@ -265,75 +261,6 @@ build_extras () {
     check_error_dialog "Building extras"
 }
 
-build_ncurses () {
-    cd ${SOURCEDIR}
-    
-    cd ncurses-${NCURSES_VERSION}
-    
-    if [ -f Makefile ] ; then
-        make -j ${JFLAG} clean
-    fi
-    sed -i '/LIBTOOL_INSTALL/d' c++/Makefile.in
-    CFLAGS="${CFLAGS}" ./configure \
-        --prefix=/usr \
-        --with-termlib \
-        --with-terminfo-dirs=/lib/terminfo \
-        --with-default-terminfo-dirs=/lib/terminfo \
-        --without-normal \
-        --without-debug \
-        --without-cxx-binding \
-        --with-abi-version=5 \
-        --enable-widec \
-        --enable-pc-files \
-        --with-shared \
-        CPPFLAGS=-I$PWD/ncurses/widechar \
-        LDFLAGS=-L$PWD/lib \
-        CPPFLAGS="-P"
-
-    make CROSS_COMPILE=$CROSS_COMPILE64 ARCH=$ARCH64 -j ${JFLAG}
-    make CROSS_COMPILE=$CROSS_COMPILE64 ARCH=$ARCH64 install -j ${JFLAG}  \
-        DESTDIR=${ROOTFSDIR}
-    check_error_dialog "ncurses-${NCURSES_VERSION}"
-}
-
-build_nano () {
-    cd ${SOURCEDIR}
-
-    cd nano-${NANO_VERSION}
-    if [ -f Makefile ] ; then
-            make -j ${JFLAG} clean
-    fi
-    CFLAGS="${CFLAGS}" ./configure \
-        --prefix=/usr \
-        LDFLAGS=-L$PWD/lib
-
-    make CROSS_COMPILE=$CROSS_COMPILE64 ARCH=$ARCH64 -j ${JFLAG}
-    make CROSS_COMPILE=$CROSS_COMPILE64 ARCH=$ARCH64 install -j ${JFLAG} \
-        DESTDIR=${ROOTFSDIR}
-
-    check_error_dialog "nano-${NANO_VERSION}"
-}
-
-build_vim () {
-    cd ${SOURCEDIR}
-
-    cd vim${VIM_DIR}
-    if [ -f Makefile ] ; then
-            make -j ${JFLAG} clean
-    fi
-    CFLAGS="${CFLAGS}" ./configure \
-        --prefix=/usr \
-        LDFLAGS=-L$PWD/lib
-
-    make CROSS_COMPILE=$CROSS_COMPILE64 ARCH=$ARCH64 -j ${JFLAG}
-    make CROSS_COMPILE=$CROSS_COMPILE64 ARCH=$ARCH64 install \
-        -j ${JFLAG} \
-        DESTDIR=${ROOTFSDIR}
-
-    check_error_dialog "vim-${VIM_VERSION}"
-}
-
-
 generate_rootfs () {	
     cd ${ROOTFSDIR}
     rm -f linuxrc
@@ -368,10 +295,10 @@ generate_rootfs () {
 
     touch motd
     echo >> motd
-    echo ' ------------------------------------ 2019.4 ' >> motd
+    echo ' ----------------------$DISTRIBUTION_VERSION ' >> motd
     echo '                   "..^__                    ' >> motd
     echo '                   *,,-,_).-~                ' >> motd
-    echo '                 LIGHT LINUX                 ' >> motd
+    echo '                 LIGHT LINUX x86_64          ' >> motd
     echo '                                             ' >> motd
     echo '  ------------------------------------------ ' >> motd
     echo >> motd
